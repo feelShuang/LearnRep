7ea48275c6d256184fd583c1e4abcc412c299ce0
liushuang <liuswin@163.com>
2017-05-25 00:00:18 +0800

    direct complete

--- learnAngularJS/js/controller/helloAngular_directive.js
+++ learnAngularJS/js/controller/helloAngular_directive.js
@@ -4,5 +4,15 @@ angular.module('myApp', [])
 	$rootScope.rootProperty = 'root scope';
 })
 .controller('ParentController', function($scope){
-	
+	// 使用.controller访问‘ng-controller’内部的属性
+	// 在DOM忽略的$scope中，根据当前控制器进行推断
+	$scope.parentProperty = 'parent scope';
+})
+.controller('ChildController', function(){
+	$scope.rootProperty = 'child scope';
+	// 同在DOM中一样，我们可以通过当前$scope直接访问原型中的任意属性
+	$scope.fullSentenceFromChild = 'Same $scope: We can access: ' + 
+		$scoep.rootProperty + 'and' + 
+		$scope.parentProperty + 'and' + 
+		$scope.childProperty
 })
